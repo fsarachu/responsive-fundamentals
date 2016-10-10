bba3f4cc72d7fd98347e85086d30272d92ccebad
Franco Sarachu <fsarachu@gmail.com>
2016-10-10 08:41:07 -0300

    Add gulp.watch to trigger reload

--- 3 - Applying Patterns/gulpfile.js
+++ 3 - Applying Patterns/gulpfile.js
@@ -1,11 +1,14 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 
 // Static server
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
+
   browserSync.init({
     server: {
       baseDir: "./"
     }
   });
+
+  gulp.watch("./*.{html,css,js}").on('change', browserSync.reload);
 });
