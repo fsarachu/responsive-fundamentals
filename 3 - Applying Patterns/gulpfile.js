61ef5542c64d2682af59194d9ab491a9d0f6026b
Franco Sarachu <fsarachu@gmail.com>
2016-10-10 16:14:15 -0300

    Update browser-sync task

--- 3 - Applying Patterns/gulpfile.js
+++ 3 - Applying Patterns/gulpfile.js
@@ -10,7 +10,9 @@ gulp.task('browser-sync', function () {
     }
   });
 
-  gulp.watch("**/*.{html,css,js}").on('change', browserSync.reload);
+  gulp.watch("./*.html").on('change', browserSync.reload);
+  gulp.watch("css/*.css").on('change', browserSync.reload);
+  gulp.watch("js/*.js").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['browser-sync']);
