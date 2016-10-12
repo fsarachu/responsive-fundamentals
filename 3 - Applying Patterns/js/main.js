8a703250557d24547fc8edb5cd6fd6a93f6f48df
Franco Sarachu <fsarachu@gmail.com>
2016-10-12 09:22:45 -0300

    Set up dotdotdot plugin. Now that beautiful multi line ellipsis is working!

--- 3 - Applying Patterns/js/main.js
+++ 3 - Applying Patterns/js/main.js
@@ -1,8 +1,26 @@
-/* Navigation control */
 $(function () {
+
+  /* Navigation control */
 
   $('#nav__toggle').on('click', function () {
     $('#main-nav').toggleClass('hidden');
+  });
+
+
+  /* Dotdotdot plugin configuration*/
+
+  $(".snippet__text").dotdotdot({
+    ellipsis: '... ',
+    wrap: 'word',
+    fallbackToLetter: true,
+    after: null,
+    watch: true,
+    height: null,
+    tolerance: 0,
+    lastCharacter: {
+      remove: [' ', ',', ';', '.', '!', '?'],
+      noEllipsis: []
+    }
   });
 
 });
